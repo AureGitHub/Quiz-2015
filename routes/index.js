4c3466bced705e070cfc88fe54b2ab0e573ff178
AureGitHub <auredecocccam@gmail.com>
2015-06-07 23:55:05 +0200

    Aure. Borrar pregunta

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,8 @@ router.post('/quizes/create',              quizController.create);
 
 router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
 router.put('/quizes/:quizId(\\d+)',        quizController.update);
+router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
 
-router.get('/quizes/:search?',quizController.index);
+router.get('/quizes/:search?',             quizController.index);
 
 module.exports = router;
