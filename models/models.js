1318c6dc6b68fa9b6f932b83855e145694dc85c4
AureGitHub <auredecocccam@gmail.com>
2015-06-07 13:18:21 +0200

    Aure. Coleccion de preguntas

--- models/models.js
+++ models/models.js
@@ -23,7 +23,7 @@ var sequelize = new Sequelize(DB_name, user, pwd,
     host:     host,
     storage:  storage,  // solo SQLite (.env)
     omitNull: true      // solo Postgres
-  }      
+  }
 );
 
 // Importar definicion de la tabla Quiz
@@ -37,10 +37,11 @@ sequelize.sync().then(function() {
   // then(..) ejecuta el manejador una vez creada la tabla
   Quiz.count().then(function (count){
     if(count === 0) {   // la tabla se inicializa solo si está vacía
-      Quiz.create({ pregunta: 'Capital de Italia',
-      	            respuesta: 'Roma'
-      	         })
-      .then(function(){console.log('Base de datos inicializada')});
+      Quiz.bulkCreate(
+        [ {pregunta: 'Capital de Italia',   respuesta: 'Roma'},
+          {pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}
+        ]
+      ).then(function(){console.log('Base de datos inicializada')});
     };
   });
 });
